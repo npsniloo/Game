5f6a8626ff4ecf75cf6b34a19761503295d7aa93
npsniloo <niloofar.pahlevan@gmail.com>
2025-10-22 18:59:40 +0330

    framework track

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: iImr1yBp */
+/* Manifest version: B0noBDzt */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
